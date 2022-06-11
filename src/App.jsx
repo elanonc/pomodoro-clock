c2dd177aaecbdbfa6d6eaae2559f316bbb85d75b
Elano N. Caitano <nunes.elano14@gmail.com>
2022-06-11 16:54:01 -0300

    Fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Pomodoro from './Components/Pomodoro';
 function App() {
 
   return (
-    <div className="App">
+    <div className="App text-center">
       <Pomodoro />
     </div>
   )
 }
 
-export default App
+export default App;
